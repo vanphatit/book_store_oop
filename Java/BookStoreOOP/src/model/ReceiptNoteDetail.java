954dc37824e3809c8256196e1f14015434df9090
Phat Lee <vanphat15it@gmail.com>
2023-11-30 11:04:07 +0700

    c#: create models + init data | java: rename ReceiptNoteDetails

---
+++ Java/BookStoreOOP/src/model/ReceiptNoteDetail.java
@@ -0,0 +1,47 @@
+package model;
+
+public class ReceiptNoteDetail {
+    private int unitPrice;
+    private int quantity;
+    private ReceiptNote receiptNote;
+    private Book book;
+
+    public ReceiptNoteDetail(int unitPrice, int quantity, ReceiptNote receiptNote, Book book) {
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.receiptNote = receiptNote;
+        this.book = book;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ReceiptNote getReceiptNote() {
+        return receiptNote;
+    }
+
+    public void setReceiptNote(ReceiptNote receiptNote) {
+        this.receiptNote = receiptNote;
+    }
+
+    public Book getbook() {
+        return book;
+    }
+
+    public void setbook(Book book) {
+        this.book = book;
+    }
+}
